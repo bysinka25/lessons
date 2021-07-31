3a8780230f16b846ac036efad2ed66a0b3ef2c9d
bysinka25 <kirgizilla@gmail.com>
2021-07-31 23:30:20 +0300

    feat(index.html, main.js) add var, create html

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+var moNey = 1;
+var inCome = 2;
+var addExpenses = 3;
+var dePosit = 4;
+var misSion = 5;
+var peRiod = 6;
+alert("Привет, это мой первый урок!");
+console.log("Первая попытка");
